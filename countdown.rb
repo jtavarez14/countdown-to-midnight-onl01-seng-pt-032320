a3f3c16ac5f1b155e99b5e516763cc12287a42df
JeanCarlos Tavarez <jeancarlos.tavarez1@gmail.com>
2020-03-29 05:34:07 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,4 +10,14 @@ def countdown(n)
 end
 
 
-
+def countdown_with_sleep(n)
+ n = 10 
+ while n > 0 
+   puts "#{n} SECOND(S)!"
+   n -= 1 
+   sleep 1
+ if n == 0
+   puts "HAPPY NEW YEAR!"
+ end
+ end
+end
